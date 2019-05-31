15f9d11e51ef1c911158d425516e137388575864
TrentB <trentjblackwell@gmail.com>
2019-05-31 06:24:39 -0700

    fifth try

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+if (process.env.NODE_ENV !== 'production') { require('dotenv').config(); };
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
